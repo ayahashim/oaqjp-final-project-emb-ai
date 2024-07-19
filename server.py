25ef07c1b118526b8174264f4fdcf828111984ec
ayahashim <ayahashim16@gmail.com>
2024-07-19 11:45:48 -0400

    Emotion Detection web app

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/emotionDetector")
+
+def emotion_detection():
+
+    response = request.arge.get["textToAnalyze"]
+    anger_score = response["anger"]
+    disgust_score = response["disgust"]
+    fear_score  = response["fear"]
+    joy_score  = response["joy"]
+    sadness_score  = response["sadness"]
+    dominant_detector  = response["dominant_emotion"]
+    
+    if dominant_detector == None:
+        return "Invalid text! Please try again!."
+    else:
+        return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {dominant_detector}."
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5000)
